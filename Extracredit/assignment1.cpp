3dfe564f0ebed98e713b5bd62b9623e7201f060c
Mynor <gattogt490@gmail.com>
2026-08-25 00:03:57 -0700

    class skeleton

---
+++ Extracredit/assignment1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+class Number_bucket{
+  private:
+    int arr[];
+
+  public:
+    Number_bucket(int size){
+      
+    }
+    ~Number_bucket(){
+
+    }
+    void push(int number){
+
+    }
+    int pop(){
+      return 0;
+    }
+    bool isEmpty(){
+
+    }
+    int getSize(){
+      return 0;
+    }
+};
+
+int main()
+{
+}
